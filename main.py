e011c6f8cdd50b6af5e35ea92b2eac6866d339a5
Vladislav Pavlovich Protasov <167005444+maxwhiter@users.noreply.github.com>
2024-04-21 17:28:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+from pygame import *
+'''Необходимые классы'''
+
+
+#класс-родитель для спрайтов
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y > 5:
+            self.rect.y+= self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y > 5:
+            self.rect.y+= self.speed
+
+class Ball(GameSprite):
+    def update(self):
+        self.rect.x  += self.speed
+        self.rect.y  += self.speed
+        if self.rect.y <5 :
+            self.speed *= -1
+        elif self.rect.y > 495:
+            self.speed *= -1
+
+
+# racket1  = Playe("racket.png", 20, 200, 4, 50, )
+#игровая сцена:
+back = (200, 255, 255) #цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+
+#флаги, отвечающие за состояние игры
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
